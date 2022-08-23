8500a1a0f7b588566d8e439e05c64f9d9a8a69d0
Johnson <grream@909c4ac83ddf.ant.amazon.com>
2022-08-23 12:00:24 -0600

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "",     // CognitoClientID
-  "api_base_url": "",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "",                   // CognitoDomainName
-  "redirect_url": ""                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "54qnqr3578vb5h9urbmo95aij2",     // CognitoClientID
+  "api_base_url": "https://egvuy3otia.execute-api.us-east-1.amazonaws.com/prod",                                     // TodoFunctionApi
+  "cognito_hosted_domain": "mytodoappdemo-ab3teststack.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
+  "redirect_url": "https://master.d3w2asdraqnmhm.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
